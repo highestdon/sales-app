79018876f8c72d3d3fc367aaf05f01b4579ce26e
Nathaniel <nathanielduodu98@gmail.com>
2026-05-30 01:43:44 +0000

    Log Mongo database name

--- backend/config/db.js
+++ backend/config/db.js
@@ -11,20 +11,3 @@ function connectMongo() {
 
 module.exports = { connectMongo };
 
-const mongoose = require('mongoose');
-
-function connectMongo() {
-  const uri = process.env.MONGO_URI;
-
-  if (!uri) {
-    throw new Error('MONGO_URI is not set');
-  }
-
-  mongoose.set('strictQuery', true);
-
-  return mongoose.connect(uri).then(() => {
-    console.log('Connected DB:', mongoose.connection.name);
-  });
-}
-
-module.exports = { connectMongo };
